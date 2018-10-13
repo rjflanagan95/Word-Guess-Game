e131436e09b0eb3b9841b9d1b103b77d4a44f910
Robert Flanagan <robertflanagan95@gmail.com>
2018-10-12 20:32:47 -0400

    added bootstrap sty styling, theme details

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // the computer will pick a random word 
 var userOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerOptions = ["appetite", "burgers", "pinata", "clothesline"];
+var computerOptions = ["buffalo", "cheetah", "elephant", "gazelle", "hippo", "leopard", "lion", "rhino", "zebra"];
 var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
 // for verifying functionality
@@ -62,11 +62,35 @@ function checkArrays(arr1, arr2) {
     return true;
 }
 
+// function to display animal photo after a win
+function displayPhoto(animal) {
+    if (animal == "buffalo") {
+        animalImage.src = "assets/images/buffalo.jpg"
+    } else if (animal == "cheetah") {
+        animalImage.src = "assets/images/cheetah.jpg"
+    } else if (animal == "elephant") {
+        animalImage.src = "assets/images/elephant.jpg"
+    } else if (animal == "gazelle") {
+        animalImage.src = "assets/images/gazelle.jpg"
+    } else if (animal == "hippo") {
+        animalImage.src = "assets/images/hippo.jpg"
+    } else if (animal == "leopard") {
+        animalImage.src = "assets/images/leopard.jpg"
+    } else if (animal == "lion") {
+        animalImage.src = "assets/images/lion.jpg"
+    } else if (animal == "rhino") {
+        animalImage.src = "assets/images/rhino.jpg"
+    } else if (animal == "zebra") {
+        animalImage.src = "assets/images/zebra.jpg"
+    }
+}
+
 // assigning variables to the HTML elements we're changing
 var winsText = document.getElementById("wins");
 var wordDisplay = document.getElementById("word-display");
 var attemptsText = document.getElementById("guesses-left");
 var guessesText = document.getElementById("already-guessed");
+var animalImage = document.getElementById("animalImg");
 
 for (var i = 0; i < computerChoice.length; i++) {
     hiddenWord[i] = "-";
@@ -109,6 +133,7 @@ document.onkeyup = function(event) {
         if (checkArrays(hiddenWord, computerWord)) {
             numWins += 1;
             winsText.textContent = numWins;
+            displayPhoto(computerChoice);
             resetGame();
         }
         // conditions for a loss
